dc0eee30de1c99d8b794ec37de0f04bf7ca3ac60
David <davidbrundukov228@gmail.com>
2025-05-23 16:22:59 +0300

    send main.py

--- main.py
+++ main.py
@@ -121,8 +121,7 @@ async def get_transcription2(pack):
     print(f'Создана папка: {folder}')
 
     # Объедините команды в одну строку
-    cookies_path = cookies_path = "./cookies.txt"
-
+    cookies_path = os.path.join(script_dir, 'cookies.txt')
     cmd = (
         f'"{yt_dlp_path}" --cookies "{cookies_path}" --ffmpeg-location "{script_dir}" '
         f'--write-auto-subs --sub-lang ru,en --skip-download "{url}"'
